d44a0690e8c09131b2a6df39ed08a8cda323d0ad
chenghao <616915054@qq.com>
2018-04-27 23:27:39 +0800

    学习 建造者模式

---
+++ Builder/Parts/Vehicle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DesignPatterns\Creational\Builder\Parts;
+
+
+abstract class Vehicle
+{
+
+    private $data = [];
+
+    public function setPart($key,$value)
+    {
+        $this->data[$key] = $value;
+    }
+
+}
